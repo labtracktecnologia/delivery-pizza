c2c6cbfc068b8de7c98e17edc04d81008742caa9
andre <andre.bongiolo@projuris.com>
2020-02-20 14:27:40 -0300

    reestruturação do projeto

--- src/main/java/com/labtrackensino/javaweb/autentication/SecurityConfig.java
+++ src/main/java/com/labtrackensino/javaweb/autentication/SecurityConfig.java
@@ -1,15 +1,13 @@
 package com.labtrackensino.javaweb.autentication;
 
-import com.labtrackensino.javaweb.autentication.filters.JWTAuthorizationFilter;
 import com.labtrackensino.javaweb.autentication.filters.JWTAuthenticationFilter;
+import com.labtrackensino.javaweb.autentication.filters.JWTAuthorizationFilter;
 import com.labtrackensino.javaweb.autentication.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -22,11 +20,11 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
-@Configuration
+
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Qualifier("inMemoryUserDetailsManager")
 	@Autowired
 	private UserDetailsService userDetailsService;
 
